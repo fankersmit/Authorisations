89c07473f6cbebcdad729222bb755bd578b130c5
FritsA <frits.ankersmit@norne.nl>
2020-11-08 13:46:28 +0100

    Initial commit

---
+++ Controllers/Authorisation.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Authorisation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class AuthorisationController : ControllerBase
+    {
+        private readonly ILogger<AuthorisationController> _logger;       
+        
+        public AuthorisationController(ILogger<AuthorisationController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("under-consideration")]
+        public Dictionary<string,int> RequestsUnderConsideration()
+        {
+            return new Dictionary<string,int> {{"RequestsUnderConsideration", 0 }};
+        }
+    }
+}
